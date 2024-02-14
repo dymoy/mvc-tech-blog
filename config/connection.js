babf8593278e961913d8c965a03e94f68b5f4661
dymoy <148160802+dymoy@users.noreply.github.com>
2024-02-14 11:44:31 -0800

    Adding JawsDB env. to connections

--- config/connection.js
+++ config/connection.js
@@ -11,16 +11,20 @@ require('dotenv').config();
 // Create the Sequelize instance 
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASSWORD,
-  {
-    host: 'localhost',
-    dialect: 'mysql',
-    port: 3306,
-  }
+if (process.env.JAWSDB_URL) {
+	sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+	const sequelize = new Sequelize(
+		process.env.DB_NAME,
+		process.env.DB_USER,
+		process.env.DB_PASSWORD,
+  	{
+		host: 'localhost',
+		dialect: 'mysql',
+		port: 3306,
+  	}
 );
+}
 
 // Export the Sequelize instance to be used in ../server.js
 module.exports = sequelize;
